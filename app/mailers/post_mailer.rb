2cc198ecdf475f70d24d1ba163b49f61001fc514
Silas Owings <silasowings@gmail.com>
2015-04-20 21:31:47 -0400

    added gmail sntp

---
+++ app/mailers/post_mailer.rb
@@ -0,0 +1,11 @@
+class PostMailer < ActionMailer::Base
+
+  def post_created(user)
+    mail(to: user.email,
+         from: "services@gmail.com",
+         subject: "Post created",
+         body: "This is my first Mailer!"
+    )
+  end
+
+end
--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,17 @@ module ActionMail
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    ActionMailer::Base.smtp_settings = {
+ 
+        :address        => 'smtp.gmail.com',
+        :domain         => 'mail.google.com',
+        :port           => 587,
+        :user_name      => "mymailerapp@gmail.com", #ENV['GMAIL_USERNAME'],
+        :password       => "weakpass", #ENV['GMAIL_PASSWORD'],
+        :authentication => 'login',
+        :enable_starttls_auto => true
+ 
+    }
   end
 end
